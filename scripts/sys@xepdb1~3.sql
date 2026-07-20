01afc3d3376cf21a7a1c144631f2c55ad144e449
luvatoshei <luvatoshe@gmail.com>
2026-07-20 16:42:21 +0300

    create loyalty_system scheme and grants

---
+++ scripts/sys@xepdb1~3.sql
@@ -0,0 +1,17 @@
+-- Creating 'loyalty' scheme
+
+create user loyalty_system
+    identified by loyalty1
+    default tablespace users
+    temporary tablespace temp
+    quota unlimited on users
+    account unlock;
+
+-- Grant premission to create objects 
+
+grant connect,
+      resource,
+      create view,
+      create materialized view,
+      create procedure,
+      create sequence to loyalty_system;
